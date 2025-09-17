a6bd7fa845cfd7e8e10eba86c04a6d3de52c2837
paolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-17 20:21:54 -0300

    Exercício 1.1: Variáveis e Console

---
+++ exercicio-1.1.js
@@ -0,0 +1,9 @@
+//Exercício 1.1: Variáveis e Console
+let meuNome = "Paola";
+let minhaIdade = 17;
+let minhaCidade = "Pomerode - SC";
+let gostaDeProgramar = true;
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log("Gosta de programar: ",gostaDeProgramar);
