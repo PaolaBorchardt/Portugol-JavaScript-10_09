f3bd59871c1554a315a9abc6d50eac1f685b3e8e
paolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-17 20:27:03 -0300

    Exercício 1.3: Condicionais Básicas

---
+++ exercicio-1.3.js
@@ -0,0 +1,24 @@
+//Exercício 1.3: Condicionais Básicas
+
+let soma = 0;
+
+for (let i = 1; i <= 10; i++) {
+    soma += i; // soma = soma + i
+}
+
+console.log(`A soma dos números de 1 a 10 é: ${soma}`);
+
+// Complete a função que verifica se um número é par ou ímpar
+
+function verificarParImpar(numero) {
+    if (numero % 2 === 0) {
+        return "par";
+    } else {
+        return "ímpar";
+    }
+}
+
+
+// Teste a função
+console.log(verificarParImpar(8));  // deve retornar "par"
+console.log(verificarParImpar(7));  // deve retornar "ímpar"
